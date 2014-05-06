0f1e73190a46b3053bb9b8972a9a9e56fd9f98c2
gavra90 <gavra90@gmail.com>
2014-05-06 15:26:54 -0700

    firist commit creating web page

--- project.clj
+++ project.clj
@@ -3,4 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring "1.0.1"]
+                 [compojure "1.0.1"]
+                 [enlive "1.0.0"]
+                 [noir "1.3.0-alpha10"]
+                 [clj-json "0.5.3"]]
+  :cljsbuild {:builds
+              [{:builds nil,
+                :jar true
+                :source-path "src/AirportRoutes/cljs"
+                :compiler {:output-dir "resources/public/js/"
+                           :output-to "resources/public/js/main.js"
+                           :optimization :simple
+                           :pretty-print true}}]})
---
+++ src/AirportRoutes/site.clj
@@ -0,0 +1,21 @@
+(ns AirportRoutes.site)
+
+  (:use (compojure handler 
+                   [core :only (GET POST defroutes)]))
+  
+  (:require compojure.route 
+            [net.cgrand.enlive-html :as en]
+            [ring.util.response :as response] 
+            [ring.adapter.jetty :as jetty]
+            [clj-json.core :as json] )
+  
+  (en/deftemplate homepage
+    (en/xml-resource "homepage.html")
+    [request])
+  
+  (def server (jetty/run-jetty #'app {:port 8080 :join false}))
+  
+ 
+  
+  
+  (def app (compojure.handler/site app*))
