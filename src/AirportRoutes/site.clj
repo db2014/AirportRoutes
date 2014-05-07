7e94eb6e7c0dbdc2b1a6d4c321ed6c755a8546d4
gavra90 <gavra90@gmail.com>
2014-05-07 08:53:48 -0700

    put some data on page dynamically

--- src/AirportRoutes/site.clj
+++ src/AirportRoutes/site.clj
@@ -8,25 +8,32 @@
           [ring.adapter.jetty :as jetty]
           [clj-json.core :as json]))
 
-(en/deftemplate homepage
-  (en/xml-resource "homepage.html")
-  [request])
+
+(defonce list-names (atom {}))
+(defonce counter (atom 0))
+
+(defn add-to-list [name]
+  (swap! list-names assoc (swap! counter inc) name ))
+
+
 
 (def server (jetty/run-jetty #'app {:port 8088 :join false}))
 
 (en/deftemplate homepage
-  (en/xml-resource "homepage.html")
-  [request])
+  (en/xml-resource "homepage.html")  
+  [request]
+  [:#list :li] (en/clone-for [[id name] @list-names]                               
+                             (en/content (format "%s : %s" id name))))
 
 
 (defroutes app*
   (GET "/" request (homepage request))
   (POST "/test" request 
-          #_{:status 200
-          :body (with-out-str (print request))
-          :headers {"Content-Type" "text/plain"}}
-        (let [n (-> request :params :number)]
-          (response/redirect (str n)))))
+        #_{:status 200
+           :body (with-out-str (print request))
+           :headers {"Content-Type" "text/plain"}}
+        (add-to-list (-> request :params :number))
+        (homepage request)))
 
 
 (def app (compojure.handler/site app*))
