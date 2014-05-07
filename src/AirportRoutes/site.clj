4d736dd0446b57bb6e3990372acc941ec3977410
Gavra <gavra90@gmail.com>
2014-05-07 20:14:09 +0200

    added two combo boxes, text field for range of plane... filled combo boxes with airport data. fn data-for-combo rearange raw airport data.

--- src/AirportRoutes/site.clj
+++ src/AirportRoutes/site.clj
@@ -1,39 +1,42 @@
 (ns AirportRoutes.site
 (:use (compojure handler 
                  [core :only (GET POST defroutes)]))
-
 (:require compojure.route 
+          [AirportRoutes.core :as dajks]
           [net.cgrand.enlive-html :as en]
           [ring.util.response :as response] 
           [ring.adapter.jetty :as jetty]
           [clj-json.core :as json]))
 
+(def airpor-data dajks/data)
 
-(defonce list-names (atom {}))
-(defonce counter (atom 0))
+(defn data-for-combo [airpor-data]
+  (reduce (fn [acc [id m]]
+            (assoc acc id (str (:name m)" " (:city m)" " (:country m))))
+          {0 "Select airport"} airpor-data))
 
-(defn add-to-list [name]
-  (swap! list-names assoc (swap! counter inc) name ))
-
-
+(def combo-data (data-for-combo airpor-data))
 
 (def server (jetty/run-jetty #'app {:port 8088 :join false}))
 
 (en/deftemplate homepage
-  (en/xml-resource "homepage.html")  
+  (en/xml-resource "homepage.html")
   [request]
-  [:#list :li] (en/clone-for [[id name] @list-names]                               
-                             (en/content (format "%s : %s" id name))))
+   [:#od :option] (en/clone-for[[v t] combo-data]
+                          [:option]
+                          (en/content t)
+                          [:option]
+                          (en/set-attr :value (str v)))
+    [:#do :option] (en/clone-for[[v t] combo-data]
+                          [:option]
+                          (en/content t)
+                          [:option]
+                          (en/set-attr :value (str v))))
 
 
 (defroutes app*
   (compojure.route/resources "/")
-  (GET "/" request (homepage request))
-  (POST "/testAjax" request
-        (let [p1 (-> request :params :a) 
-              p2 (-> request :params :b)]        
-          (add-to-list p1))
-        (homepage request)))
+  (GET "/" request (homepage request)))
 
 
 (def app (compojure.handler/site app*))
