1407228905d050c3af9ccca41c67b9d154d64026
gavra90 <gavra90@gmail.com>
2014-05-08 08:48:46 -0700

    return json solution

--- src/AirportRoutes/site.clj
+++ src/AirportRoutes/site.clj
@@ -8,8 +8,16 @@
           [ring.adapter.jetty :as jetty]
           [clj-json.core :as json]))
 
+(defn json-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/json"}
+   :body (json/generate-string data)})
 (def airpor-data dajks/data)
 
+(defn id-to-xy [solution] 
+  (reduce (fn [acc e]              
+            (conj acc  (str (:logitude (airpor-data e)) "*" (:lantitude (airpor-data e)) "*"(:name (airpor-data e)))))
+          [] solution))
 (defn data-for-combo [airpor-data]
   (reduce (fn [acc [id m]]
             (assoc acc id (str (:name m)" " (:city m)" " (:country m))))
@@ -22,12 +30,12 @@
 (en/deftemplate homepage
   (en/xml-resource "homepage.html")
   [request]
-   [:#od :option] (en/clone-for[[v t] combo-data]
+   [:#from :option] (en/clone-for[[v t] combo-data]
                           [:option]
                           (en/content t)
                           [:option]
                           (en/set-attr :value (str v)))
-    [:#do :option] (en/clone-for[[v t] combo-data]
+    [:#to :option] (en/clone-for[[v t] combo-data]
                           [:option]
                           (en/content t)
                           [:option]
@@ -36,7 +44,19 @@
 
 (defroutes app*
   (compojure.route/resources "/")
-  (GET "/" request (homepage request)))
+  (POST "/callDajkst" request
+        #_{:status 200
+           :body (with-out-str (print request))
+          :headers {"Content-Type" "text/plain"}}        
+        (let [odA (-> request :params :odA)
+              doB (-> request :params :doB)
+              range (-> request :params :txtRange)
+              solution (id-to-xy (dajks/dajks-alg (java.lang.Integer/parseInt odA) (java.lang.Integer/parseInt doB)(java.lang.Integer/parseInt range)))]
+          (json-response  solution)))
+  (GET "/" request (homepage request))
+  (compojure.route/not-found "ERROR"))
 
 
 (def app (compojure.handler/site app*))
+
+
